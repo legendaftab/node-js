7a3dd73f4fca334b52f49de29d19e2f67cae67b9
aftab kazi <aftabkazi533@gmail.com>
2024-08-26 13:43:55 +0530

    Ignore node_modules

---
+++ figlets/index.js
@@ -0,0 +1,10 @@
+var figlet = require("figlet");
+
+figlet(" kazi sir 786!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
---
+++ script.js
@@ -0,0 +1,18 @@
+import {sum,PI} from "./math.js"
+console.log(sum(2,4));
+
+
+
+
+// ){
+//     console.log("hello bhai",args[i]);
+// }
+
+// const sumValue = require("./math");
+// console.log(sumValue);
+// console.log(sumValue.sum(4,4))
+
+// const info = require("./fruits");
+// console.log(info);
+
+// 
